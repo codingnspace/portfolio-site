9dd41e19fcee76d9c373ae50202d0a00927f0a71
Pearl McPhee <Pearl.McPhee@siriusxm.com>
2019-08-14 16:00:47 -0400

    added styling for projects

--- src/components/project.js
+++ src/components/project.js
@@ -1,35 +1,39 @@
 import React from "react"
-import Image from "gatsby-image"
 import { rhythm } from "../utils/typography"
+import styles from './project.module.css'
 
-const Project = (props) => {
-console.log(props)
-    const contributions = props.project.contributions.map((contribution, idx) => {
-        return <li key={idx+contribution}>{contribution}</li>
-    })
-    return (
-        <div>
-            <Image
-                alt={`Screenshot of ${props.project.name}`}
-                style={{
-                    marginRight: rhythm(1 / 2),
-                    marginBottom: 0,
-                    minWidth: 50,
-                    borderRadius: `100%`,
-                }}
-                fixed={props.project.screenshot}
-                imgStyle={{
-                    borderRadius: `50%`,
-                }}
-            />
-            <h3>{props.project.name}</h3>
-            <p>{props.project.overview}</p>
-            <ul>{contributions}</ul>
-            <h4>{`Tech used: ${props.project.tech}`}</h4>
-            <a href={props.project.github}>View on Github</a>
-            {props.project.demo && <div><a href={props.project.demo}>Demo</a></div>}
-        </div>
-    )
+class Project extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            isExpanded: false
+        }
+    }
+
+    handleClick = () => {
+        this.setState({
+            isExpanded: !this.state.isExpanded
+        })
+    }
+
+    render() {
+        const contributions = this.props.project.contributions.map((contribution, idx) => {
+            return <li key={idx+contribution}>{contribution}</li>
+        })
+        return (
+            <div className={`${styles.project} ${this.state.isExpanded && styles.isExpanded}`} onClick={this.handleClick}>
+                <img src={this.props.project.screenshot} />
+                <div className={styles.projectContent}>
+                    <h3>{this.props.project.name}</h3>
+                    <p>{this.props.project.overview}</p>
+                    {/* <ul>{contributions}</ul> */}
+                    <h4>{`Tech used: ${this.props.project.tech}`}</h4>
+                </div>
+                <a href={this.props.project.github}>View on Github</a>
+                {this.props.project.demo && <div><a href={this.props.project.demo}>Demo</a></div>}
+            </div>
+        )
+   }
 }
 
 export default Project
--- src/components/projects.js
+++ src/components/projects.js
@@ -20,10 +20,8 @@ const Projects = props => {
     const projectData = [
         {
             name: 'Reach Mobile',
-            screenshot: `${data.reach.childImageSharp.fixed}`,
-            overview: `Reach Mobile is a Progressive Web Application built primarily with React, JavaScript (ES6), JSX and Apex. Supplemental libraries included: Redux, React-Redux, React Router, Reselect, ImmutableJS, Webpack as the task runner and Enzyme for front-end unit testing. 
-    
-            The app is currently being freely offered to hospital systems Health Leads partners with as a tool to connect low income patients with local life enhancing resources.`,
+            screenshot: 'https://www.hellomagazine.com/imagenes//healthandbeauty/health-and-fitness/2019072575722/beyonce-weight-loss-new-details-revealed/0-367-633/beyonce-weight-loss-revealed-t.jpg',
+            overview: `Reach Mobile is a Progressive Web Application currently being used to connect low income patients with local life enhancing resources.`,
             contributions: [`Built new features such as the Reach for Providers feature that expanded the use of the patient app to include the scenario where a patient navigator uses the app on behalf of the patient and therefore included added in case management abilities such as find and selecting a patient, adding resources to a particular patient's case, updating a patient's demographic data and searching for resources by keywords. To build this feature, I collaborated with members from the Product Design Team to land on a UX that matched the pre-existing app as well the provider's unique needs.`,
                             `Created multiple iterations of the resource cards that displays different information depending on if the card is in the search results, detail or preview modes`,
                             `Conducted multiple user testing interviews at a clinic in California, engaged with over 100 patients, asked questions from a survey that I helped create and recorded their responses.`,
@@ -32,7 +30,7 @@ const Projects = props => {
             tech: 'React, ES6, Redux, ImmutableJS, and Webpack'
         }, {
             name: 'DC Weather App',
-            screenshot: '',
+            screenshot: 'https://www.hellomagazine.com/imagenes/healthandbeauty/mother-and-baby/2019081276411/beyonce-twins-rare-appearance-with-jay-z/0-370-489/beyonce-twins-rare-appearance-t.jpg',
             overview: 'This is a sample app that uses React, Redux and the DarkSky API. It can be used to determine if someone should take the metro or bike to work given their own weather parameters.',
             contributions: ['I both designed and implemented this app.'],
             github: 'https://github.com/pearl1991/gigantum-weather-app',
